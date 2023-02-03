8161d2733e0616f0e0d95e220c24228a886bb9ba
bison <stinus.petersen@ufst.dk>
2023-02-03 14:10:34 +0100

    wip

--- ticketauth/src/main/kotlin/dk/ufst/ticketauth/automated/AutomatedAuthEngine.kt
+++ ticketauth/src/main/kotlin/dk/ufst/ticketauth/automated/AutomatedAuthEngine.kt
@@ -1,14 +1,10 @@
 package dk.ufst.ticketauth.automated
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.net.Uri
 import android.os.Handler
 import android.os.Looper
 import android.util.Base64
-import android.util.Log
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -21,19 +17,7 @@ import dk.ufst.ticketauth.authcode.AuthJob
 import dk.ufst.ticketauth.log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import net.openid.appauth.AuthState
-import net.openid.appauth.AuthorizationException
-import net.openid.appauth.AuthorizationResponse
-import net.openid.appauth.AuthorizationService
-import net.openid.appauth.AuthorizationServiceConfiguration
-import net.openid.appauth.EndSessionRequest
-import net.openid.appauth.EndSessionResponse
-import net.openid.appauth.TokenRequest
-import net.openid.appauth.TokenResponse
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -41,16 +25,11 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.Charset
-import java.util.concurrent.locks.Condition
-import java.util.concurrent.locks.ReentrantLock
+import java.time.Instant
 
 internal class AutomatedAuthEngine(
-    context: Context,
     private val sharedPrefs: SharedPreferences,
-    private val dcsBaseUrl: String,
     private val clientId: String,
-    private val scopes: String,
-    private val redirectUri: String,
     private val onNewAccessToken: OnNewAccessTokenCallback,
     private val onAuthResultCallback: OnAuthResultCallback,
     private val tokenUrl: String,
@@ -58,61 +37,43 @@ internal class AutomatedAuthEngine(
     private val nonce: String,
     private val provider: AutomatedAuthConfig.Provider,
 ): AuthEngine {
-    private var authService: AuthorizationService = AuthorizationService(context)
-    private var serviceConfig : AuthorizationServiceConfiguration
-    var authState: AuthState = AuthState()
 
+    data class AuthState (
+        var accessToken: String,
+        var tokenExpTime : Instant
+    )
     override val roles = mutableListOf<String>()
-
-    private val refreshLock = ReentrantLock()
-    private val refreshCondition: Condition = refreshLock.newCondition()
     var activityProvider: ActivityProvider = null
     private var startForResultAuth: ActivityResultLauncher<Intent?>? = null
-    private var startForResultLogout: ActivityResultLauncher<Intent?>? = null
+    private var authState: AuthState? = null
+
     override var onWakeThreads: ()->Unit = {}
-
     override val jobs: MutableMap<Int, AuthJob> = mutableMapOf()
-
     override val accessToken: String?
-        get() = authState.accessToken
+        get() = authState?.accessToken
+    override val isAuthorized: Boolean = authState != null
 
-    override val isAuthorized: Boolean
-        get() = authState.isAuthorized
 
     override fun needsTokenRefresh(): Boolean = authState.needsTokenRefresh
     private val scope = CoroutineScope(Dispatchers.IO)
 
 
     init {
-        serviceConfig = buildServiceConfig()
         // deserialize authstate if we have one, otherwise start with a fresh
-        sharedPrefs.getString("authState", null)?.let {
+        sharedPrefs.getString("accessToken", null)?.let {
             log("Loading existing auth state")
-            authState = AuthState.jsonDeserialize(it)
-            decodeJWT()
+            decodeJWT(it)
         } ?: run {
             log("Creating a new auth state")
         }
         //log("packageName: ${context.packageName}")
     }
 
-    private fun buildServiceConfig() =
-        AuthorizationServiceConfiguration(
-            Uri.parse("${dcsBaseUrl}${AUTH_PATH}"),  // authorization endpoint
-            Uri.parse("${dcsBaseUrl}${TOKEN_PATH}"), // token endpoint
-            null,
-            Uri.parse("${dcsBaseUrl}${LOGOUT_PATH}")
-        )
-
     override fun installActivityProvider(activityProvider: ActivityProvider) {
         this.activityProvider = activityProvider
         startForResultAuth =
             activityProvider!!.invoke().registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
                 //processAuthResult(result)
-            }
-        startForResultLogout =
-            activityProvider.invoke().registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-                processLogoutResult(result)
             }
     }
 
@@ -176,8 +137,6 @@ internal class AutomatedAuthEngine(
                     processTokenResponse(jsonResponse)
                 } catch (t : Throwable) {
                     log("Token endpoint called failed with exception: ${t.message}")
-                    val r : TokenResponse? = null
-                    authState.update(r, AuthorizationException.TokenRequestErrors.INVALID_REQUEST)
                     wakeThreads(AuthResult.ERROR)
                     t.printStackTrace()
                 }
@@ -190,105 +149,18 @@ internal class AutomatedAuthEngine(
     }
 
     override fun launchLogoutIntent() {
-        log("Launching logout intent")
-        authState.idToken?.let {
-            val endSessionRequest = EndSessionRequest.Builder(serviceConfig)
-                .setIdTokenHint(it)
-                .setPostLogoutRedirectUri(Uri.parse(redirectUri))
-                .build()
-            val endSessionIntent = authService.getEndSessionRequestIntent(endSessionRequest)
-            try {
-                startForResultLogout!!.launch(endSessionIntent)
-            } catch (t : Throwable) {
-                log("Cannot launch logout intent due to exception: ${t.message}")
-                wakeThreads(AuthResult.ERROR)
-            }
-        } ?: run {
-            log("Cannot launch logout intent because we have no idToken")
-            wakeThreads(AuthResult.ERROR)
-        }
+        log("Logout is not supported with automated login")
+        wakeThreads(AuthResult.ERROR)
     }
 
 
     private fun processTokenResponse(tokenResponse: JSONObject) {
         log("processTokenResponse ${tokenResponse.toString(4)}")
-        val request = TokenRequest.Builder(serviceConfig, clientId)
-            .setAuthorizationCode("AUTHCODE")
-            .setRedirectUri(Uri.parse(redirectUri))
-            .build()
-        val  response = TokenResponse.Builder(request)
-            .fromResponseJson(tokenResponse)
-            .build()
-        authState.update(response, null)
+        decodeJWT(tokenResponse.getString("access_token"))
+        log("Token expiration: ${authState?.tokenExpTime}")
+        persistAuthState()
+        onAccessToken()
         wakeThreads(AuthResult.SUCCESS)
-
-        /*
-        if(result.resultCode == Activity.RESULT_CANCELED) {
-            log("Auth flow was cancelled by user")
-            wakeThreads(AuthResult.CANCELLED_FLOW)
-        } else {
-            result.data?.let { data ->
-                val resp = AuthorizationResponse.fromIntent(data)
-                val ex = AuthorizationException.fromIntent(data)
-                authState.update(resp, ex)
-                persistAuthState()
-                if (resp != null) {
-                    // authorization completed
-                    performTokenRequest(resp)
-                    log("Got auth code: ${resp.authorizationCode}")
-                } else {
-                    log("Auth failed: $ex")
-                    // user cancelled login flow (in flow cancel option), wake threads so they can return error
-                    wakeThreads(AuthResult.ERROR)
-                }
-            } ?: run {
-                log("ActivityResult yielded no data (intent) to process")
-                wakeThreads(AuthResult.ERROR)
-            }
-        }
-         */
-    }
-
-    private fun processLogoutResult(result: ActivityResult) {
-        log("processLogoutResult $result")
-        if(result.resultCode == Activity.RESULT_CANCELED) {
-            wakeThreads(AuthResult.CANCELLED_FLOW)
-            return
-        }
-        result.data?.let { data ->
-            val resp = EndSessionResponse.fromIntent(data)
-            val ex = AuthorizationException.fromIntent(data)
-            if (resp != null) {
-                log("Completed logout")
-                clear()
-                wakeThreads(AuthResult.SUCCESS)
-            } else {
-                wakeThreads(AuthResult.ERROR)
-                log("Logout failed: $ex")
-            }
-        } ?: run {
-            wakeThreads(AuthResult.ERROR)
-            log("ActivityResult yielded no data (intent) to process")
-        }
-
-    }
-
-    private fun performTokenRequest(authResp: AuthorizationResponse) {
-        log("performTokenRequest")
-        authService.performTokenRequest(authResp.createTokenExchangeRequest()) { resp, ex ->
-            authState.update(resp, ex)
-            persistAuthState()
-            if (resp != null) {
-                // exchange succeeded
-                log("Got access token: ${resp.accessToken}")
-                decodeJWT()
-                onAccessToken()
-                wakeThreads(AuthResult.SUCCESS)
-            } else {
-                log("Token exchange failed: $ex")
-                wakeThreads(AuthResult.ERROR)
-            }
-        }
     }
 
     private fun wakeThreads(result: AuthResult) {
@@ -311,49 +183,20 @@ internal class AutomatedAuthEngine(
         }
     }
 
-    /**
-    Use a reentrant lock to wait for the callback from createTokenRefreshRequest blocking
-    the function from returning until completion. This is necessary because that appauth
-    library doesn't provide a synchronous way of making the call
-     */
+    // Token refresh is not supported with automated login
     override fun performBlockingTokenRefresh(): Boolean {
-        var success = false
-        refreshLock.lock()
-        try {
-            val req = authState.createTokenRefreshRequest()
-            authService.performTokenRequest(req) { resp, ex ->
-                authState.update(resp, ex)
-                persistAuthState()
-                if (resp != null) {
-                    success = true
-                    decodeJWT()
-                    onAccessToken()
-                } else {
-                    log("Token refresh exception $ex")
-                }
-                refreshLock.lock()
-                try {
-                    refreshCondition.signal()
-                } finally {
-                    refreshLock.unlock()
-                }
-            }
-            refreshCondition.await()
-        } catch (t: Throwable) {
-            Log.e("TicketAuth", "Token refresh failed, no refresh token", t)
-            return false
-        } finally {
-            refreshLock.unlock()
-        }
-        return success
+        return false
     }
 
     private fun persistAuthState() {
-        sharedPrefs.edit().putString("authState", authState.jsonSerializeString()).apply()
+        authState?.let {
+            sharedPrefs.edit().putString("accessToken", it.accessToken).apply()
+            sharedPrefs.edit().putLong("tokenExpTime", it.tokenExpTime.epochSecond).apply()
+        }
     }
 
     private fun onAccessToken() {
-        onNewAccessToken?.invoke(authState.accessToken!!)
+        onNewAccessToken?.invoke(authState!!.accessToken)
     }
 
     override fun runOnUiThread(block: ()->Unit) {
@@ -361,40 +204,34 @@ internal class AutomatedAuthEngine(
     }
 
     override fun clear() {
-        authState = AuthState()
+        authState = null
         persistAuthState()
     }
 
-    private fun decodeJWT() {
-        authState.accessToken?.let { accessToken ->
-            val split = accessToken.split(".")
-            val header = String(Base64.decode(split[0], Base64.URL_SAFE), Charset.forName("UTF-8"))
-            val body = String(Base64.decode(split[1], Base64.URL_SAFE), Charset.forName("UTF-8"))
-            val headerJson = JSONObject(header)
-            val bodyJson = JSONObject(body)
+    private fun decodeJWT(accessToken : String) {
+        val split = accessToken.split(".")
+        val header = String(Base64.decode(split[0], Base64.URL_SAFE), Charset.forName("UTF-8"))
+        val body = String(Base64.decode(split[1], Base64.URL_SAFE), Charset.forName("UTF-8"))
+        val headerJson = JSONObject(header)
+        val bodyJson = JSONObject(body)
 
-            log("Token Decoded Header: ${headerJson.toString(4)}")
-            log("Token Decoded Body: ${bodyJson.toString(4)}")
-            roles.clear()
-            if (bodyJson.has("realm_access")) {
-                val rolesJson = bodyJson.getJSONObject("realm_access").getJSONArray("roles")
-                rolesJson.let { ar ->
-                    for (i in 0 until ar.length()) {
-                        val role = ar.getString(i)
-                        roles.add(role)
-                    }
+        log("Token Decoded Header: ${headerJson.toString(4)}")
+        log("Token Decoded Body: ${bodyJson.toString(4)}")
+        val exp : Long = bodyJson.getLong("exp")
+
+        authState = AuthState(accessToken = accessToken, tokenExpTime = Instant.ofEpochSecond(exp))
+
+        roles.clear()
+        if (bodyJson.has("realm_access")) {
+            val rolesJson = bodyJson.getJSONObject("realm_access").getJSONArray("roles")
+            rolesJson.let { ar ->
+                for (i in 0 until ar.length()) {
+                    val role = ar.getString(i)
+                    roles.add(role)
                 }
             }
         }
     }
 
-    override fun destroy() {
-        authService.dispose()
-    }
-
-    companion object {
-        private const val AUTH_PATH: String = "/protocol/openid-connect/auth"
-        private const val TOKEN_PATH: String = "/protocol/openid-connect/token"
-        private const val LOGOUT_PATH: String = "/protocol/openid-connect/logout"
-    }
+    override fun destroy() {}
 }
